ac05d28bfcec178bae0493b9c438bb919c6fcc1e
Wildan <winistein@gmail.com>
2019-06-29 12:41:57 +0700

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+  'updatechecker' => false,
+  'instanceid' => 'ocmltqyqr4k7',
+  'passwordsalt' => 'pqFbKDKN1xxoLd3kYg3y9vuvlQqrvK',
+  'secret' => 'Y+2LsoZ4vqjZQYCw4zkFrma9GjQH7QBjxi35aDThDvoIZ/+k',
+  'trusted_domains' =>
+  array (
+    0 => 'selasarakademik.id',
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'overwrite.cli.url' => 'https://selasarakademik.id',
+  'dbtype' => 'mysql',
+  'version' => '10.1.0.4',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'owncloud',
+  'dbpassword' => '@Akademik102@',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,1,0,4);
+$OC_VersionString = '10.1.0';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2019-02-06T14:11:38+00:00 f74542bf29b18dc78715f4334bd64dcfd1102322';
+$vendor = 'owncloud';
